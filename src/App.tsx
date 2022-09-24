63a1bd90cfbd8c43467f9efcfc5985e8819df3a5
aezazali1997 <aezazali.pro@gmail.com>
2022-09-24 12:15:48 +0500

    feat:Op-85: add scrollbar for content scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React, { Suspense, lazy } from 'react'
-import { form } from './Services/formValidation';
+import { form } from './Services/formValidation'
 const Home = lazy(() => import('@components/Home/Home'));
-const AboutMe = lazy(() => import('@components/AboutMe/AboutMe'));
-const ContactMe = lazy(() => import('@components/ContactMe/ContactMe'));
+const AboutMe = lazy(() => import('@components/AboutMe/AboutMe'))
+const ContactMe = lazy(() => import('@components/ContactMe/ContactMe'))
 const Cursor = lazy(() => import('@components/Cursor/Cursor'));
 const Navbar = lazy(() => import('@components/Navbar/Navbar'))
-const Services = lazy(() => import('@components/Services/Services'));
-import Loading from '@components/Loading/Loading';
+const ScrollBar = lazy(() => import('@components/ScrollBar/ScrollBar'))
+const Services = lazy(() => import('@components/Services/Services'))
+const Projects = lazy(() => import('@components/Projects/Projects'))
+/**
+ * ! Do not make it lazy load
+ * * This is a fallback it needs to be loaded immediatly
+ **/
+import Loading from '@components/Loading/Loading'
 
-const App = () => {
-  return (
-    <Suspense fallback={<Loading />}>
+const App = () => (
+  <Suspense fallback={<Loading />}>
+    <ScrollBar>
       <Cursor />
       <Navbar />
       <Home />
+      <Projects />
       <Services />
       <AboutMe />
       <ContactMe form={form} />
-    </Suspense>
-  )
-}
+    </ScrollBar>
+  </Suspense>
+)
 
 export default App
