dfde021cb6540869e808ec19b1836bce371e23b5
zubair-bin-hamid <52604695+zubair-bin-hamid@users.noreply.github.com>
2024-07-09 18:06:10 +0500

    card responsiveness added

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -29,7 +29,7 @@ const Home = () => {
         </div>
         <span className={classes.start}> Start /&gt;</span>
         <h2 className={classes.intro}>
-          Hi, my name is <span className=" text-black bg-black hover:bg-black">Aezaz Ali</span>
+          Hi, my name is <span className="bg-white">Aezaz Ali</span>
         </h2>
         <h2 className={classes.more}>
           i <em>design</em> and <span className={classes.develop}>develop</span>
